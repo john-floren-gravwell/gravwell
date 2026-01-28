3c0d14c0b3434c281f95ec25d4225d106c81d0c2
John Floren <john@jfloren.net>
2026-01-28 15:54:26 -0800

    Revert "Merge pull request #2055 from john-floren-gravwell/registry-secrets"
    This reverts commit 17b83aee94c7289b384e1bb85267312c265a5394, reversing
    changes made to cbe6b2d8c5c5fb9f4ebd5cc29f8ee77995d820cb.

--- client/types/secrets.go
+++ client/types/secrets.go
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright 2026 Gravwell, Inc. All rights reserved.
+ * Copyright 2021 Gravwell, Inc. All rights reserved.
  * Contact: <legal@gravwell.io>
  *
  * This software may be modified and distributed under the terms of the
@@ -8,28 +8,34 @@
 
 package types
 
-// Secret is the metadata associated with a secret. It contains
-// ownership information but not the associated value. This is the
-// primary type that will be returned by standard user APIs; the SecretFull
-// type is only returned via a special searchagent-specific endpoint.
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Secret is the metadata associated with a secret, it contains ownership information but not the associated value
 type Secret struct {
-	CommonFields
+	ID      uuid.UUID `json:"id"`
+	Name    string    `json:"name"`
+	Desc    string    `json:"description"`
+	UID     int32     `json:"uid"`
+	Groups  []int32   `json:"groups,omitempty"`
+	Global  bool      `json:"global"`
+	Created time.Time `json:"created"`
 }
 
-// SecretCreate is the structure used to ask the API to make a new secret.
+// SecretCreate is the structure used to ask the API to make a new secret, only the request parameters are present
 type SecretCreate struct {
-	CommonFields
-	Value string
+	Name   string  `json:"name"`
+	Desc   string  `json:"description"`
+	Groups []int32 `json:"groups,omitempty"`
+	Global bool    `json:"global"`
+	Value  string  `json:"value"`
 }
 
-// SecretFull is returned only to the searchagent through a special endpoint.
+// SecretFull represents the full secret including its value.  This type is not sent through any traditional APIs
 type SecretFull struct {
-	CommonFields
-	Value string
-}
-
-// SecretListResponse is returned when listing secrets.
-type SecretListResponse struct {
-	BaseListResponse
-	Results []Secret `json:"results"`
+	Secret
+	Value string `json:"value"`
 }
